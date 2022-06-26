eb5388b013ac64781c1e91e574b4603e6f371435
Vero0102 <104280568+Vero0102@users.noreply.github.com>
2022-06-26 18:58:35 +0300

    Course work 1 is ended

--- src/pro/sky/java/course1/Employee.java
+++ src/pro/sky/java/course1/Employee.java
@@ -1,2 +1,66 @@
-package pro.sky.java.course1;public class Employee {
+package pro.sky.java.course1;
+
+public class Employee {
+
+    private static int counter = 1;
+
+    private final String name;
+    private final String surname;
+    private final String patronymicName;
+    private double salary;
+    private int department;
+    private final int id;
+
+    public Employee(String name, String surname, String patronymicName, double salary, int department) {
+        this.id = counter++;
+        this.name = name;
+        this.surname = surname;
+        this.patronymicName = patronymicName;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPatronymicName() {
+        return patronymicName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ". " +
+                "ФИО: " + surname + " " +
+                name + " " +
+                patronymicName + ". " +
+                "Зарплата: " + salary + ". " +
+                "Отдел: " + department +
+                ".";
+    }
 }
+
--- src/pro/sky/java/course1/Main.java
+++ src/pro/sky/java/course1/Main.java
@@ -2,7 +2,87 @@ package pro.sky.java.course1;
 
 public class Main {
 
+    private static final Employee[] EMPLOYEES = new Employee[10];
+
     public static void main(String[] args) {
-	// write your code here
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            EMPLOYEES[0] = new Employee("Marina", "Romanova", "Aleksandrovna", 75002.1, 1);
+            EMPLOYEES[1] = new Employee("Anton", "Pavlov", "Ivanovich", 85562.5, 1);
+            EMPLOYEES[2] = new Employee("Roman", "Ivanov", "Petrovich", 63762.25, 2);
+            EMPLOYEES[3] = new Employee("Tigran", "Konev", "Vladimirovich", 69562.55, 2);
+            EMPLOYEES[4] = new Employee("Lev", "Arbuzov", "Vladimirovich", 55562.5, 3);
+            EMPLOYEES[5] = new Employee("Oleg", "Ruben", "Vladimirovich", 57562, 3);
+            EMPLOYEES[6] = new Employee("Larisa", "Ivanova", "Ivanovna", 53562.5, 4);
+            EMPLOYEES[7] = new Employee("Andzhelika", "Mordashova", "Vladimirovna", 54562.5, 4);
+            EMPLOYEES[8] = new Employee("Olga", "Vinogradova", "Petrovna", 45475.75, 5);
+            EMPLOYEES[9] = new Employee("Nadezhda", "Volkova", "Andreevna", 47671.65, 5);
+        }
+        listOfAllEmployees();
+        System.out.println();
+        double totalMonthSalaries = totalMonthSalaries();
+        System.out.println("Затраты компании на зарплату сотрудников за месяц: " + totalMonthSalaries);
+        Employee employeeWithMinimalSalary = minSalaryEmployee();
+        System.out.println("Сотрудник с минимальной месячной зарплатой: " + employeeWithMinimalSalary);
+        Employee employeeWithMaximalSalary = maxSalaryEmployee();
+        System.out.println("Сотрудник с максимальной месячной зарплатой: " + employeeWithMaximalSalary);
+        double averageMonthSalaryOfAllEmployees = averageMonthSalaryOfAllEmployees();
+        System.out.printf("Средняя месячная зарплата всех сотрудников: %.2f", averageMonthSalaryOfAllEmployees);
+        System.out.println();
+        System.out.println();
+        listFullNamesOfAllEmployees();
+    }
+
+    private static void listOfAllEmployees() {
+        for (Employee employee : EMPLOYEES) {
+            System.out.println(employee);
+        }
+    }
+    private static double totalMonthSalaries() {
+        double sum = 0;
+        for (Employee employee : EMPLOYEES) {
+            sum += employee.getSalary();
+        }
+        return sum;
+    }
+
+    private static Employee minSalaryEmployee() {
+        double minSalary = Double.MAX_VALUE;
+        int index = -1;
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            if (EMPLOYEES[i].getSalary() < minSalary) {
+                minSalary = EMPLOYEES[i].getSalary();
+                index = i;
+            }
+        }
+        if(index != -1){
+            return EMPLOYEES[index];
+        } else {
+            return null;
+        }
+    }
+
+    private static Employee maxSalaryEmployee() {
+        double maxSalary = Double.MIN_VALUE;
+        int index = -1;
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            if (EMPLOYEES[i].getSalary() > maxSalary) {
+                maxSalary = EMPLOYEES[i].getSalary();
+                index = i;
+            }
+        }
+        if(index != -1){
+            return EMPLOYEES[index];
+        } else {
+            return null;
+        }
+    }
+    private static double averageMonthSalaryOfAllEmployees() {
+        return totalMonthSalaries() / EMPLOYEES.length;
+    }
+    private static void listFullNamesOfAllEmployees() {
+        for (Employee employee : EMPLOYEES) {
+            System.out.println(employee.getSurname() + " " + employee.getName() + " " + employee.getPatronymicName());
+        }
     }
 }
+
